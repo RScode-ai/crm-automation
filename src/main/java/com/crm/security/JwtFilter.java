8789c796542a3aad3bc30c9bd5f86c38ae5e7030
Rohit Sharma <rohitsharma05824@gmail.com>
2026-06-27 12:43:03 +0530

    JWT filter update

--- src/main/java/com/crm/security/JwtFilter.java
+++ src/main/java/com/crm/security/JwtFilter.java
@@ -14,6 +14,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 
+
+
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -29,8 +31,11 @@ public class JwtFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain)
             throws ServletException, IOException {
+        System.out.println("JWT FILTER EXECUTED");
 
         System.out.println("REQUEST PATH = " + request.getServletPath());
+
+
 
         String authHeader = request.getHeader("Authorization");
 
